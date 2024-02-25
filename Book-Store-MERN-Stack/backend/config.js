8ad7ddbdc1794285b6a3037e254db8011a089775
Ujjwal Narayan <narayanujjwal1@gmail.com>
2024-02-25 19:04:18 +0530

    first commit

---
+++ Book-Store-MERN-Stack/backend/config.js
@@ -0,0 +1,7 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+"mongodb://0.0.0.0:27017/book-store";
+
+// Please create a free database for yourself.
+// This database will be deleted after tutorial
